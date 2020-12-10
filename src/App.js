6ede5340da712dcfab6679144741977ec0325bb1
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2020-12-10 13:14:27 +0530

    Questions Uploaded and basic logic done

--- src/App.js
+++ src/App.js
@@ -8,30 +8,35 @@ var questions = [
     ans: 0,
     response:
       "Dorami is the cute sister robot of Doraemon who lives with Sewashi (Nobita's great grandson in the 22nd century). She occasionally helps Nobita and is shown as a more advanced robot than Doraemon."
+  },
+
+  {
+    quest: "What is the name of bully Gian's sister?",
+    options: ["A. Jaiko", "B. Shizuka", "C. Dorami"],
+    ans: 0,
+    response:
+      "Jaiko is the fat ugly sister of Gian whom Gian loves very much. She loves to create mangas and enter them in contests. Her pen name is Christine Goda. She loves her pet dog very much, even though Gian sometimes gets annoyed with this fact."
+  },
+
+  {
+    quest: "Who is Gian's sidekick?",
+    options: ["A. Nobita", "B. Suneo", "C. Dorami"],
+    ans: 1,
+    response:
+      "Suneo is shown to be a wealthy kid who remains with Gian only to not be bullied by him. He actually likes Nobita and admits it often, much to the chagrin of Gian."
+  },
+  {
+    quest: "Nobita fears more?",
+    options: ["A.  Mom", "B. Jian", "C. Confusing Afff!"],
+    ans: 1,
+    response: "Confusing enough, but close observation says this! 😁"
   }
-
-  // {
-  //   quest: "What is the name of bully Gian's sister? \nA. Jaiko\nB. Shizuka\nC. Dorami\n",
-  //   ans:["Jaiko","jaiko","a","A"],
-  //   response: "Jaiko is the fat ugly sister of Gian whom Gian loves very much. She loves to create mangas and enter them in contests. Her pen name is Christine Goda. She loves her pet dog very much, even though Gian sometimes gets annoyed with this fact.",
-  // },
-
-  // {
-  //   quest: "Who is Gian's sidekick? \nA. Nobita\nB. Suneo\n",
-  //   ans:["Suneo","suneo","B","b"],
-  //   response: "Suneo is shown to be a wealthy kid who remains with Gian only to not be bullied by him. He actually likes Nobita and admits it often, much to the chagrin of Gian.",
-  // },
-  // {
-  //   quest: "Nobita fears more?\nA. Mom\nB. Jian\nConfusing Afff!\n",
-  //   ans: ["Jian","jian","Mom","mom","a","A","b","B"],
-  //   response: "I am not sure of the answer so BOTH ARE TRUE! 😁"
-  // },
 ];
 
 export default function App() {
-  const [quest, setQuest] = useState("");
   const [options, setOptions] = useState([]);
   const [score, setScore] = useState(0);
+  const [correct, setCorrect] = useState("");
   function responseSelect(optionNo) {
     setOptions(optionNo);
   }
@@ -39,9 +44,9 @@ export default function App() {
   function checkResponse(correctResponse) {
     if (correctResponse === options) {
       setScore(score + 1);
-      console.log("Correct");
+      setCorrect("Correct");
     } else {
-      console.log("Oops! This is Wrong!");
+      setCorrect("Oops! Wrong");
     }
   }
 
@@ -56,6 +61,8 @@ export default function App() {
             <button onClick={() => responseSelect(1)}>{dict.options[1]}</button>
             <button onClick={() => responseSelect(3)}>{dict.options[2]}</button>
             <button onClick={() => checkResponse(dict.ans)}>Submit</button>
+            <h3>{correct}</h3>
+
             <h3>Current Score: {score}</h3>
           </div>
         );
